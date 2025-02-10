23165853543b9de3770394487e88f2994ecaead3
akshay singh <akshaysinghrathore171@gmail.com>
2025-02-10 16:37:39 +0530

    add number

---
+++ basic/Add.java
@@ -0,0 +1,7 @@
+public class Add {
+    public static void main(Grade2[] args) {
+        int num1=10,num2=5,solution;
+        solution=num1+num2;
+        System.out.println("the value of c is "+solution);
+    }
+}
