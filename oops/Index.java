011afbcd755cbb360c118dc0ab416f22e794f66d
akshay singh <akshaysinghrathore171@gmail.com>
2025-03-18 13:09:56 +0530

    First code

---
+++ oops/Index.java
@@ -0,0 +1,12 @@
+package oops;
+
+public class Index {
+    int x = 5;
+  
+    public static void main(String[] args) {
+      Index myObj1 = new Index();  // Object 1
+      Index myObj2 = new Index();  // Object 2
+      System.out.println(myObj1.x);
+      System.out.println(myObj2.x);
+    }
+  }
